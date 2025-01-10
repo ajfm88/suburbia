e241dad1ae687809309811e14ab9ca78cbf49ee9
ajfm88 <ale@ajfm88.com>
2025-01-10 11:32:07 -0800

    03. Create the Homepage.

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,21 +1,22 @@
-import {
-  SliceSimulator,
-  SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
+import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-export default async function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const { state } = await searchParams;
-  const slices = getSlices(state);
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
 
-  return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
-    </SliceSimulator>
-  );
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
