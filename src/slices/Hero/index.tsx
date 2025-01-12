ed60f634c278ff6524ebc245531029b69af6206e
ajfm88 <ale@ajfm88.com>
2025-01-12 09:57:38 -0800

    04. Adding Media from the resources page.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -2,6 +2,7 @@ import { Content } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
+import { Bounded } from "@/components/Bounded";
 /**
  * Props for `Hero`.
  */
@@ -12,14 +13,15 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="border"
     >
       <PrismicRichText field={slice.primary.heading} />
       <PrismicRichText field={slice.primary.body} />
       <PrismicNextLink field={slice.primary.button} />
-    </section>
+    </Bounded>
   );
 };
 
