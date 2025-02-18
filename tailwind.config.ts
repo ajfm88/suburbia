0593b91a3a8229d85428b6c2e810d5cda9abc35d
ajfm88 <ale@ajfm88.com>
2025-02-18 07:09:25 -0800

    06. Building the Hero (Part 2).

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract } from "fluid-tailwind";
+import fluid, {
+  extract,
+  screens,
+  fontSize,
+  FluidThemeConfig,
+} from "fluid-tailwind";
 
 export default {
   content: {
@@ -12,10 +17,24 @@ export default {
     extract,
   },
   theme: {
+    fluid: (({ theme }) => ({
+      defaultScreens: ["20rem", theme("screens.lg")],
+    })) satisfies FluidThemeConfig,
+    screens,
+    fontSize,
     extend: {
       fontFamily: {
         sans: ["var(--font-bowlby-sc)"],
         mono: ["var(--font-dm-mono)"],
+      },
+      colors: {
+        "brand-blue": "#4876ff",
+        "brand-lime": "#d9f154",
+        "brand-navy": "#2e3192",
+        "brand-orange": "#ff7347",
+        "brand-pink": "#f7d0e9",
+        "brand-purple": "#692e54",
+        "brand-gray": "#fffdf9",
       },
     },
   },
