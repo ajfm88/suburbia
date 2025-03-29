065fc133df4877d0ef3c7b1a6249f3d5f2a0d487
ajfm88 <ale@ajfm88.com>
2025-03-29 10:56:11 -0700

    49. Create React Context to handle state.

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -4,25 +4,42 @@ import { Logo } from "@/components/Logo";
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
+import { CustomizerControlsProvider } from "./context";
+import { createClient } from "@/prismicio";
 
-export default async function Page({}: Props) {
+export default async function Page() {
+  const client = createClient();
+  const customizerSettings = await client.getSingle("board_customizer");
+  const { wheels, decks, metals } = customizerSettings.data;
+
+  const defaultWheel = wheels[0];
+  const defaultDeck = decks[0];
+  const defaultTruck = metals[0];
+  const defaultBolt = metals[0];
+
   return (
     <div className="flex min-h-screen flex-col lg:flex-row">
-      <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
-        <Link href="/" className="absolute left-6 top-6">
-          <Logo className="h-12 text-white" />
-        </Link>
-      </div>
-      <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:w-96 lg:shrink-0 lg:grow-0">
-        <Heading as="h1" size="sm" className="mb-6 mt-0">
-          Build your board
-        </Heading>
+      <CustomizerControlsProvider
+        defaultWheel={defaultWheel}
+        defaultDeck={defaultDeck}
+        defaultTruck={defaultTruck}
+        defaultBolt={defaultBolt}
+      >
+        <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
+          <Link href="/" className="absolute left-6 top-6">
+            <Logo className="h-12 text-white" />
+          </Link>
+        </div>
+        <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:w-96 lg:shrink-0 lg:grow-0">
+          <Heading as="h1" size="sm" className="mb-6 mt-0">
+            Build your board
+          </Heading>
 
-        <ButtonLink href="" color="lime" icon="plus">
-          Add to cart
-        </ButtonLink>
-      </div>
+          <ButtonLink href="" color="lime" icon="plus">
+            Add to cart
+          </ButtonLink>
+        </div>
+      </CustomizerControlsProvider>
     </div>
   );
 }
